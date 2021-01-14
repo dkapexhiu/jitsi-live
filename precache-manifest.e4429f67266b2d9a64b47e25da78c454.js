8317961e4b958aee983124970043c2556469dc12
unknown <daniel.kapexhiu@gmail.com>
2021-01-14 11:22:50 +0100

    Updates

---
+++ precache-manifest.e4429f67266b2d9a64b47e25da78c454.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff27477c2fa127633f5ef6974ad285a",
+    "url": "/jitsi-live/index.html"
+  },
+  {
+    "revision": "17514fb4907a8969a447",
+    "url": "/jitsi-live/static/css/2.a3c1bd20.chunk.css"
+  },
+  {
+    "revision": "d30c337020a170fbb3b9",
+    "url": "/jitsi-live/static/css/main.a21e9a87.chunk.css"
+  },
+  {
+    "revision": "17514fb4907a8969a447",
+    "url": "/jitsi-live/static/js/2.3deb37a5.chunk.js"
+  },
+  {
+    "revision": "5730d1823f6b27077c677030967812c2",
+    "url": "/jitsi-live/static/js/2.3deb37a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d30c337020a170fbb3b9",
+    "url": "/jitsi-live/static/js/main.95e42889.chunk.js"
+  },
+  {
+    "revision": "eb9ab22ef0b8483579dc",
+    "url": "/jitsi-live/static/js/runtime-main.387c4a3d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/jitsi-live/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/jitsi-live/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/jitsi-live/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/jitsi-live/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/jitsi-live/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
